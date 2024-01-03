1a452e7532f0271871d52e42f0297885012d4b3d
Leonardo Ferrero-Merlino <noblegas38@gmail.com>
2024-01-03 23:13:46 +0100

    Improved chat handling

--- solution/backend/express/src/routes/chat.js
+++ solution/backend/express/src/routes/chat.js
@@ -12,9 +12,9 @@ router.get('/:chatId', function(req, res) {
 
     getDb().collection('chat')
         .find({ chatId: req.params.chatId })
-        .project({ messages: { $slice: [parseInt(req.query.skip), parseInt(req.query.take)] } })
+        .project({ messages: { $slice: [parseInt(req.query.skip), parseInt(req.query.take)] }, _id: 0 })
         .toArray()
-        .then(result => { res.status(200).json(result); })
+        .then(result => { res.status(200).json(result.length > 0 ? result[0] : []); })
         .catch(err => { res.status(500).json({error: "Could not fetch the document"})})
 });
 
@@ -23,16 +23,24 @@ router.post('/save', function(req, res) {
         return res.status(400).json({error: "No body"});
     }
 
-    let messagesOrderedByDate = req.body.messages
+    let chatToBeSaved = req.body.length;
+    req.body.forEach(chat => {
+        let messagesOrderedByDate = req.body.messages
         .sort((first, second) => {return new Date(second.date) - new Date(first.date)});
 
-    getDb().collection('chat')
-        .updateOne(
-            { chatId: req.body.chatId }, 
-            { $push: { messages: { $each: messagesOrderedByDate, $position: 0 } } },
-            { upsert: true })
-        .then(result => { res.status(201).json(result); })
-        .catch(err => { res.status(500).json({error: "Could not update the chat"})})
+        getDb().collection('chat')
+            .updateOne(
+                { chatId: req.body.chatId }, 
+                { $push: { messages: { $each: messagesOrderedByDate, $position: 0 } } },
+                { upsert: true })
+            .then(result => { chatToBeSaved-- })
+            .catch(err => { res.status(500).json({error: "Could not update the chat"})})
+    });
+
+    if (chatToBeSaved === 0)
+        return res.status(201);
+    else
+        return res.status(418);
 });
 
 module.exports = router;
