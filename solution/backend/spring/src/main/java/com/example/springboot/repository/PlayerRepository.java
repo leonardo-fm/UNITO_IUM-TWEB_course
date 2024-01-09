678e3fb6a22201c5c358831cb18a908886dba04d
Leonardo Ferrero-Merlino <noblegas38@gmail.com>
2024-01-09 15:18:18 +0100

    Added order by for data spring

--- solution/backend/spring/src/main/java/com/example/springboot/repository/PlayerRepository.java
+++ solution/backend/spring/src/main/java/com/example/springboot/repository/PlayerRepository.java
@@ -11,6 +11,6 @@ public interface PlayerRepository extends JpaRepository<Player, Long> {
     @Query(value = "SELECT * FROM player WHERE player_id = :playerId", nativeQuery = true)
     Player getPlayer(long playerId);
 
-    @Query(value = "SELECT p.* FROM player AS p INNER JOIN club AS c ON p.last_season = c.last_season WHERE p.current_club_id = :clubId AND c.club_id = :clubId", nativeQuery = true)
+    @Query(value = "SELECT p.* FROM player AS p INNER JOIN club AS c ON p.last_season = c.last_season WHERE p.current_club_id = :clubId AND c.club_id = :clubId ORDER BY p.name", nativeQuery = true)
     List<Player> getClubPlayers(long clubId);
 }
