cbdb45d66d56eacc6d2f14f9e65add05b48f644a
Leonardo Ferrero-Merlino <noblegas38@gmail.com>
2024-01-10 16:33:24 +0100

    Added Run.bat -b for the build mode, build the spring anmd the angular and then start all

--- solution/Run.bat
+++ solution/Run.bat
@@ -1,9 +1,21 @@
-clear
+@echo off
+cls 
+
+set "arg1=%1"
 
 :: Start all the servers
-start cmd /k "echo Starting express server... & npm run dev --prefix .\backend\express"
-start cmd /k "echo Starting springboot server... & backend\spring\gradlew run -p backend\spring"
-start cmd /k "echo Starting main server... & npm run dev --prefix .\backend\main"
+if not defined arg1 goto :not_define 
+if "%arg1%"=="-b" ( 
+    start cmd /k "echo Starting express server... & npm run dev --prefix .\backend\express"
+    start cmd /k "echo Building springboot server... & backend\spring\gradlew build -p backend\spring & echo Starting springboot server... & backend\spring\gradlew run -p backend\spring"
+    start cmd /k "echo Starting main server... & npm run dev --prefix .\backend\main"
+    start cmd /k "echo Building frontend... & cd frontend & ng build --base-href /browser/ --output-path ../backend/main/static & exit"
+) else (
+    :not_define
+    start cmd /k "echo Starting express server... & npm run dev --prefix .\backend\express"
+    start cmd /k "echo Starting springboot server... & backend\spring\gradlew run -p backend\spring"
+    start cmd /k "echo Starting main server... & npm run dev --prefix .\backend\main"
+)
 
 :: Wait for server to start
 echo "Waiting for server to starts..."
@@ -16,4 +28,4 @@ start "" "http://localhost:3000/api-docs"
 start "" "http://localhost:8082/swagger-ui/index.html#/"
 start "" "http://localhost:3000/browser"
 
-read -p "Press any key to continue" x
+exit
