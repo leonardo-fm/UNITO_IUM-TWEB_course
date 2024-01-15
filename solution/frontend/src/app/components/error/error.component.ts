93fdef1f412ac5b2e9d9affb6df35d67e506e934
Antonio Buscema <antonio.buscema@edu.unito.it>
2024-01-15 22:55:56 +0100

    Back to home button error page

--- solution/frontend/src/app/components/error/error.component.ts
+++ solution/frontend/src/app/components/error/error.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LanguageService } from '../../services/language.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-error',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   host: { class: 'container d-flex flex-column h-100 w-100 overflow-auto py-4' },
   templateUrl: './error.component.html',
   styleUrl: './error.component.css'
