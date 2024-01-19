90235d7073f2383c11fd8d5f956a4d88bc14b8c5
Antonio Buscema <antonio.buscema@edu.unito.it>
2024-01-19 23:26:28 +0100

    Player statistics init

--- solution/frontend/src/app/components/player/player.component.ts
+++ solution/frontend/src/app/components/player/player.component.ts
@@ -9,11 +9,12 @@ import { PlayerDetailDto } from '../../models/player.dto.model';
 import { ChatRoomType } from '../../models/chat.dto.model';
 import { ScrollDirective } from '../../directives/scroll.directive';
 import { SvgDirective } from '../../directives/svg.directive';
+import { PlayerStatisticsComponent } from './player-statistics/player-statistics.component';
 
 @Component({
   selector: 'app-player',
   standalone: true,
-  imports: [PlayerGamesComponent, RouterLink, ScrollDirective, SvgDirective],
+  imports: [PlayerGamesComponent, PlayerStatisticsComponent, RouterLink, ScrollDirective, SvgDirective],
   host: { class: 'container d-flex flex-column h-100 w-100 overflow-auto gap-3 py-4' },
   templateUrl: './player.component.html',
   styleUrl: './player.component.css'
